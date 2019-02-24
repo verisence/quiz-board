dead471dd0af4a076df37ae76e9b17e4f6cb7d88
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-02-25 00:42:32 +0300

    Add an if else statement to the js file to ensure the user answers all the questions first before submiting the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
+// var quizes = [quiz1];
+var options = ["a","b","c","d"];
+
+
+
 $(document).ready(function() {
-  $("button#submit1").click(function(){
-    // var on0e = $("input:radio[name=quizOne]:checked").val();
-    // var questionOne = parseInt(on0e);
-    var questionOne=parseInt($("input:radio[name=quizOne]:checked").val());
-    var questionTwo=parseInt($("input:radio[name=quizTwo]:checked").val());
-    var questionThree=parseInt($("input:radio[name=quizThree]:checked").val());
-    var questionFour=parseInt($("input:radio[name=quizFour]:checked").val());
-    var questionFive=parseInt($("input:radio[name=quizFive]:checked").val());
-    var questionSix=parseInt($("input:radio[name=quizSix]:checked").val());
-    var questionSeven=parseInt($("input:radio[name=quizSeven]:checked").val());
-    var questionEight=parseInt($("input:radio[name=quizEight]:checked").val());
-    var questionNine=parseInt($("input:radio[name=quizNine]:checked").val());
-    var questionTen=parseInt($("input:radio[name=quizTen]:checked").val());
-    total = questionOne+questionTwo+questionThree+questionFour+questionFive+questionSix+questionSeven+questionEight+questionNine+questionTen;
-    alert(total);
-    // alert("heey");
+  $("button#submit1").click(function(event){
+
+    var questionOne=$("input:radio[name=quizOne]:checked").val();
+    var questionTwo=$("input:radio[name=quizTwo]:checked").val();
+    var questionThree=$("input:radio[name=quizThree]:checked").val();
+    var questionFour=$("input:radio[name=quizFour]:checked").val();
+    var questionFive=$("input:radio[name=quizFive]:checked").val();
+    var questionSix=$("input:radio[name=quizSix]:checked").val();
+    var questionSeven=$("input:radio[name=quizSeven]:checked").val();
+    var questionEight=$("input:radio[name=quizEight]:checked").val();
+    var questionNine=$("input:radio[name=quizNine]:checked").val();
+    var questionTen=$("input:radio[name=quizTen]:checked").val();
+    // total = questionOne+questionTwo+questionThree+questionFour+questionFive+questionSix+questionSeven+questionEight+questionNine+questionTen;
+    // // alert(total);
+    //
+    // if (total === NaN){
+    //   alert("Ensure you answer all the questions first.")
+    // }else{
+    //   // $("p#output").text("Your score is "+total+" out of 10");
+    // }
+    var total = 0;
+    var points = 10;
+    if (questionOne === undefined || questionTwo===undefined) {
+      alert("heey");
+    }else{
+      if(questionOne===options[1]){total+=points}
+      if(questionTwo===options[2]){total+=points}
+      alert(total);
+    }
+
+    event.preventDefault();
   });
-  // alert("heey");
 });
